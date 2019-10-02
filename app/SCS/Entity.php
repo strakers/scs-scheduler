f2c3104ecd966c65fcb19f4b2c18ed5716b69713
Steven Straker <contact@strakez.com>
2019-10-01 23:14:42 -0400

    Initial structure commit

---
+++ app/SCS/Entity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: strakez
+ * Date: 2019-10-01
+ * Time: 11:12 PM
+ */
+
+namespace SCS;
+
+
+class Entity
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: strakez
+ * Date: 2019-10-01
+ * Time: 11:08 PM
+ */
