80b29ba1f59db35d529ff3fdda1a3145c0559bc4
Steven Straker <contact@strakez.com>
2019-10-02 03:05:30 -0400

    Add section method to control section display for course

--- app/SCS/Entity/Course.php
+++ app/SCS/Entity/Course.php
@@ -12,4 +12,20 @@ namespace SCS\Entity;
 class Course extends BaseEntity
 {
     protected static $_key = 'd_course_code';
+
+    public function sections()
+    {
+        $sections = $this->sections;
+        foreach( $sections as &$section ){
+            $section->d_course_id = $this->d_course_id;
+            $section->d_course_code = $this->d_course_code;
+            $section->d_course_section_code = "{$this->d_course_code}-{$section->d_section_code}";
+            $section->d_course_name = $this->d_course_name;
+            unset($section->d_section_notes);
+            unset($section->instructors);
+            unset($section->d_semester_begin_date);
+            unset($section->start_month);
+        }
+        return $sections;
+    }
 }
