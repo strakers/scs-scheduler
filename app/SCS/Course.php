d1931e92b74ee52359a972e4d723b3daa7f9bcb4
Steven Straker <contact@strakez.com>
2019-10-02 00:25:03 -0400

    Create course class for retrieving course data

---
+++ app/SCS/Course.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: strakez
+ * Date: 2019-10-01
+ * Time: 11:27 PM
+ */
+
+namespace SCS;
+
+
+class Course extends Entity
+{
+    protected static $_key = 'd_course_code';
+}
