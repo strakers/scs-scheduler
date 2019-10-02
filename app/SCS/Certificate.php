c0bf68c483c1db1e5b749a7cbe578b7dad6d788a
Steven Straker <contact@strakez.com>
2019-10-02 00:32:05 -0400

    Create class for certificate

---
+++ app/SCS/Certificate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: strakez
+ * Date: 2019-10-02
+ * Time: 12:26 AM
+ */
+
+namespace SCS;
+
+
+class Certificate extends Entity
+{
+    protected static $_key = 'd_certificate_code';
+}
