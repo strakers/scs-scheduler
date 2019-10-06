ab0f46e4fb563a9a5d871a3e245f296bedcfc9e3
Steven Straker <contact@strakez.com>
2019-10-06 00:04:05 -0400

    Use modular list export method for augmenting lists

--- app/SCS/Entity/Certificate.php
+++ app/SCS/Entity/Certificate.php
@@ -13,30 +13,13 @@ class Certificate extends BaseEntity
 {
     protected static $_key = 'd_certificate_code';
 
-    /**
-     * @param string $resource_key
-     * @return array
-     */
-    public static function getList($resource_key = '' )
+    protected static function getEntityExpressPackage( $entity )
     {
-        $list = [];
-        $master_key = static::$_override_list_key ?: static::$_key;
-
-        if( $json = static::getJsonFromResource($resource_key) ){
-            if( $root = static::extractJson($json) ){
-                foreach( $root->data as $certificate ){
-                    if( static::$_key && property_exists($certificate, $master_key)){
-                        $id = $certificate->{$master_key};
-                        $list[ $id ] = [
-                            'id' => $id,
-                            'code' => $certificate->d_certificate_code,
-                            'name' => $certificate->d_certificate_name,
-                        ];
-                    }
-                }
-            }
-        }
-        return array_values($list);
+        return [
+            'id' => $entity->d_certificate_id,
+            'code' => $entity->d_certificate_code,
+            'name' => $entity->d_certificate_name,
+        ];
     }
 
     public function courses()
--- app/SCS/Entity/Course.php
+++ app/SCS/Entity/Course.php
@@ -18,6 +18,20 @@ class Course extends BaseEntity
         $sections = (array)$this->sections;
         foreach( $sections as &$section ){
             $section->d_course_section_code = "{$this->d_course_code}-{$section->d_section_code}";
+            $section->instruction_methods = implode(', ', $section->instruction_methods);
+            $section->campuses = [];
+            $section->d_course_code = $this->d_course_code;
+            $section->d_section_name = $this->d_course_name;
+            foreach($section->schedules as &$schedule){
+                $schedule->period = '';
+                if( !$schedule->d_section_schedule_is_tba && !($schedule->d_section_schedule_start_time == '00:00:00' || $schedule->d_section_schedule_end_time == '00:00:00' )){
+                    $schedule->period = !$schedule->d_section_schedule_is_tba
+                        ? "{$schedule->d_section_schedule_start_time} - {$schedule->d_section_schedule_end_time}"
+                        : '';
+                }
+                $schedule->weekday_keys = static::weekdayKeys( (array) $schedule->d_section_schedule_days );
+                if($schedule->d_campus_name) $section->campuses[] = $schedule->d_campus_name;
+            }
             unset($section->d_section_notes);
             unset($section->instructors);
             unset($section->d_semester_begin_date);
@@ -34,6 +48,40 @@ class Course extends BaseEntity
             'd_course_code' => $this->d_course_code,
             'd_course_name' => $this->d_course_name,
             'sections' => $this->sections()
+        ];
+    }
+
+    public static function weekdayKeys( array $weekdays = [] )
+    {
+        $map = [
+            'Monday' => 'M',
+            'Tuesday' => 'T',
+            'Wednesday' => 'W',
+            'Thursday' => 'R',
+            'Friday' => 'F',
+            'Saturday' => 'S',
+            'Sunday' => 'D',
+        ];
+
+        $keys = [];
+
+        if( $weekdays ) {
+            foreach ($weekdays as $i => $weekday) {
+                if (array_key_exists($weekday, $map)) {
+                    $keys[$i] = $map[$weekday];
+                }
+            }
+        }
+
+        return $keys;
+    }
+
+    protected static function getEntityExpressPackage( $entity )
+    {
+        return [
+            'id' => $entity->d_course_id,
+            'code' => $entity->d_course_code,
+            'name' => $entity->d_course_name,
         ];
     }
 }
